55a3d786828824a239bc82c68fb3f216192053f9
daihb <daihb@DESKTOP-R31MQAB>
2019-06-25 17:02:18 +0800

    适用于移动设备的后端。

---
+++ WebApiAppDemo/ToDoApiForMobile/Enums/ErrorCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApiForMobile.Enums
+{
+    public enum ErrorCode
+    {
+        TodoItemNameAndNotesRequired,
+        TodoItemIDInUse,
+        RecordNotFound,
+        CouldNotCreateItem,
+        CouldNotUpdateItem,
+        CouldNotDeleteItem
+    }
+}
---
+++ WebApiAppDemo/ToDoApiForMobile/Interfaces/IToDoRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using ToDoApiForMobile.Models;
+
+namespace ToDoApiForMobile.Interfaces
+{
+    public interface IToDoRepository
+    {
+        bool DoesItemExist(string id);
+
+        IEnumerable<ToDoItem> All { get; }
+
+        ToDoItem Find(string id);
+
+        void Insert(ToDoItem item);
+
+        void Update(ToDoItem item);
+
+        void Delete(string id);
+    }
+}
