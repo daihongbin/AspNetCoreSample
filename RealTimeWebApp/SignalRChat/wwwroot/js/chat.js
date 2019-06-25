ad9d700a1228f5eefee1bd6bf4a7b969f918cddd
Michael <Michael@DESKTOP-I89DJUH>
2019-06-25 23:36:29 +0800

    typescript的signalr

--- RealTimeWebApp/SignalRChat/wwwroot/js/chat.js
+++ RealTimeWebApp/SignalRChat/wwwroot/js/chat.js
@@ -1,5 +1,30 @@
 ﻿"use strict";
 
+//创建并启动连接
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-document.getElementById("#sendButton").disab
+document.getElementById("sendButton").disabled = true;
+
+//定义客户端的函数
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says" + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+//调用服务端函数
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+});
