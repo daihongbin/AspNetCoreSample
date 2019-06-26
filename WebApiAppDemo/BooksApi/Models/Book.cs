240c7f13f4e1a371e244d5854341664bd72147ee
daihb <daihb@DESKTOP-R31MQAB>
2019-06-26 14:11:39 +0800

    完成使用MongoDB的webapi

---
+++ WebApiAppDemo/BooksApi/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        [BsonId]//主键
+        [BsonRepresentation(BsonType.ObjectId)]//string到objectid
+        public string Id { get; set; }
+
+        [BsonElement("Name")]//指定数据库属性名称
+        [JsonProperty("Name")]
+        public string BookName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+    }
+}
