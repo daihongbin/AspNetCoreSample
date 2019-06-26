bfa0841ca39c510b5a8501d61d7a55d7b6e10ab5
daihb <daihb@DESKTOP-R31MQAB>
2019-06-26 11:21:28 +0800

    完成typescript的signalR，@aspnet/signalr要用1.1.0版本。

---
+++ RealTimeWebApp/SignalRWebPack/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWebPack.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task NewMessage(string userName,string message)
+        {
+            await Clients.All.SendAsync("messageReceived",userName,message);
+        }
+    }
+}
