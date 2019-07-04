55ed9e006811f67943a8667d6d732b5205d88619
daihb <daihb@DESKTOP-R31MQAB>
2019-07-04 15:04:07 +0800

    数据访问->EF Core和Razor Pages->读取相关数据

---
+++ DataAccess.EFCoreAndRazorPagesDemo/ContosoUniversity/Models/SchoolViewModels/CourseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models.SchoolViewModels
+{
+    [NotMapped]
+    public class CourseViewModel
+    {
+        public int CourseID { get; set; }
+
+        public string Title { get; set; }
+
+        public int Credits { get; set; }
+
+        public string DepartmentName { get; set; }
+
+    }
+}
