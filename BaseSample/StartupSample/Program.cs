7938d4ecee7ce60667c37b496e4cff83d92fcf5d
daihb <daihb@DESKTOP-R31MQAB>
2019-08-08 17:30:00 +0800

    startup深入研究。

---
+++ BaseSample/StartupSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace StartupSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+
+                })
+                .ConfigureServices(services => 
+                {
+
+                })
+                .Configure(app => 
+                {
+                    var loggerFactory = app.ApplicationServices
+                        .GetRequiredService<ILoggerFactory>();
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    var env = app.ApplicationServices.GetRequiredService<IHostingEnvironment>();
+                });
+    }
+}
---
+++ BaseSample/StartupSample/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace StartupSample
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment _env;
+
+        private readonly IConfiguration _config;
+
+        private readonly ILoggerFactory _loggerFactory;
+
+        public Startup(IHostingEnvironment env,IConfiguration config,ILoggerFactory loggerFactory)
+        {
+            _env = env;
+            _config = config;
+            _loggerFactory = loggerFactory;
+        }
+        
+        // 这个方法主要是注册服务用的
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var logger = _loggerFactory.CreateLogger<Startup>();
+
+            if (_env.IsDevelopment())
+            {
+                logger.LogInformation("Development envirment");
+            }
+            else
+            {
+                logger.LogInformation($"Environment:{_env.EnvironmentName}");
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
