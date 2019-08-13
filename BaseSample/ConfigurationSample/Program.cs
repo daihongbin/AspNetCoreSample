e54e3f506c7bd2b8823a36384fa579562ac7d5c4
daihb <daihb@DESKTOP-R31MQAB>
2019-08-13 17:35:10 +0800

    主要是关于配置的案例。

---
+++ BaseSample/ConfigurationSample/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConfigurationSample
+{
+    public class Program
+    {
+        public static Dictionary<string, string> arrayDict = new Dictionary<string, string>
+        {
+            {"array:entries:0", "value0"},
+            {"array:entries:1", "value1"},
+            {"array:entries:2", "value2"},
+            {"array:entries:4", "value4"},
+            {"array:entries:5", "value5"}
+        };
+
+        public static readonly Dictionary<string, string> _switchMappings = new Dictionary<string, string>
+        {
+            { "-CLKey1", "CommandLineKey1" },
+            { "-CLKey2", "CommandLineKey2" }
+        };
+
+        public static void Main(string[] args)
+        {
+            //CreateWebHostBuilder(args).Build().Run();
+
+            //直接创建 WebHostBuilder 时，请使用以下配置调用 UseConfiguration
+            /*
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel()
+                .UseStartup<Startup>();
+            */
+
+            //交换映射
+            CreateSwapWebHostBuilder(args).Build().Run();
+
+            
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddInMemoryCollection(arrayDict);
+                    config.AddJsonFile("json_array.json", optional: false, reloadOnChange: false);
+                    //config.AddJsonFile("starship.json", optional: false, reloadOnChange: false);
+                    //config.AddXmlFile("tvshow.xml", optional: false, reloadOnChange: false);
+                    //config.AddEFConfiguration(options => options.UseInMemoryDataBase("InMemoryDb"));
+                    config.AddCommandLine(args);
+                })
+                .UseStartup<Startup>();
+
+        //交换映射
+        public static IWebHostBuilder CreateSwapWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder()
+            .ConfigureAppConfiguration((hostingContext,config) => 
+            {
+                config.AddCommandLine(args,_switchMappings);
+            })
+            .UseStartup<Startup>();
+
+        //环境变量配置提供程序
+        public static IWebHostBuilder CreateEnvironmenWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddEnvironmentVariables(prefix:"PREFIX_");
+            })
+            .UseStartup<Startup>();
+        
+        
+    }
+}
