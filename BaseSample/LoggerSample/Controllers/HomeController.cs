3e82b733f84a745b2e3b4e215fca8ca593750a11
daihb <daihb@DESKTOP-R31MQAB>
2019-08-16 17:22:12 +0800

    关于日志的，今天又p事没干。

---
+++ BaseSample/LoggerSample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LoggerSample.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        public IActionResult Index()
+        {
+            _logger.LogInformation("这是一条info级别的日志");
+            
+            return View();
+        }
+    }
+}
---
+++ BaseSample/LoggerSample/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace LoggerSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+
+            // 添加提供程序
+            /*
+            var webHost = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                })
+                .UseStartup<Startup>()
+                .Build();
+            
+            webHost.Run();
+            */
+
+            //这里应该是讲怎么获取logger
+            /*
+            var host = CreateWebHostBuilder(args).Build();
+
+            var todoRepository = host.Services.GetRequiredService<ITodoRepository>();
+            todoRepository.Add(new Core.Model.TodoItem() { Name = "Feed the dog" });
+            todoRepository.Add(new Core.Model.TodoItem() { Name = "Walk the dog" });
+
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("Seeded the database.");
+            
+            host.Run();
+            */
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                });
+    }
+}
