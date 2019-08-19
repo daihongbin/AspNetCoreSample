14b1b17b10d84244f8fdb076820c4f778fce4d85
daihb <daihb@DESKTOP-R31MQAB>
2019-08-19 17:36:25 +0800

    路由

---
+++ BaseSample/RouteSample/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RouteSample.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly ILogger _logger;
+        
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var link = Url.Action("Index", "Home", new {id = 17,name = "abc"});
+            _logger.LogInformation("合成的路由路径：" + link);
+            
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ BaseSample/RouteSample/ProductsLinkMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace RouteSample
+{
+    public class ProductsLinkMiddleware
+    {
+        private readonly LinkGenerator _linkGenerator;
+
+        public ProductsLinkMiddleware(RequestDelegate next,LinkGenerator linkGenerator)
+        {
+            _linkGenerator = linkGenerator;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            var url = _linkGenerator.GetPathByAction("Index", "Home");
+
+            httpContext.Response.ContentType = "text/plain";
+
+            await httpContext.Response.WriteAsync($"Go to {url} to see our products.");
+        }
+    }
+}
